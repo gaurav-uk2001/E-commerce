005159bdcae92247a4813d8830c30e4f927aef30
Gaurav Bisht <gaurav.vaishali2001@gmail.com>
2023-08-21 20:47:43 +0530

    make ,style seller signup form. Get value in funct

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./seller-auth.component.css']
 })
 export class SellerAuthComponent {
+signUp(data:object):void{
+console.warn(data);
 
+}
 }
